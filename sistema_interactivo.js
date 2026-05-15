3443e1cacfab381c12fd90758e41b71891768be5
Daniel Alvarez <danieleliazalvarezdiaz@gmail.com>
2026-05-15 17:49:26 -0500

    Refactor interactive system with product and set examples

--- sistema_interactivo.js
+++ sistema_interactivo.js
@@ -1,15 +1,69 @@
 
-/* pedir datos */
-let nombre = prompt("Colocar nombre")
-let edad = prompt("Colocar edad")
-
-/*mensajes segun la edad */
-let mensaje_menor = "Hola " + [nombre]+", eres menor de edad. ¡Sigue aprendiendo y disfrutando del código!"
-let mensaje_mayor = "Hola "+ [nombre] +", eres mayor de edad. ¡Prepárate para grandes oportunidades en el mundo de la programación!"
-let mensaje_error_dato = "hubo un error al colocar el dato de la edad cargue la pagina de nuevo y coloque un dato valido"
 
 
-/*impresion de mensaje segun la edad */
-let mensaje = edad < 18 ? alert(mensaje_menor) : edad >= 18 ? alert(mensaje_mayor) : alert(mensaje_error_dato)
 
+// objeto de productos
+let productos = [
+    {"id": 1 , "nombre": "zapato", "precio": 5000},
+    {"id": 2 , "nombre": "correa", "precio": 3000},
+    {"id": 3 , "nombre": "pantalon", "precio": 1000}
+]
+
+// creacion del set
+let numeros_repetidos = new Set([3,4,52,3,4])
+
+// numeros repetidos en el set
+console.log(numeros_repetidos)
+
+// agregar un numero al set
+let nuevo_numero = 6
+numeros_repetidos.add(Number(nuevo_numero))
+
+console.log(numeros_repetidos)
+
+// verificar si existe el numero
+let numero_verificacion = 6
+console.log(numeros_repetidos.has(numero_verificacion) ? "el numero "+ numero_verificacion +" se encuentra": "el numero "+ numero_verificacion +" no se encuentra")
+
+
+// eliminar numero de set con delete
+
+let nmero_eliminar = 6
+
+console.log(numeros_repetidos.delete(Number(nmero_eliminar)) ? "se logro eliminar el numero" : "numero "+nmero_eliminar+" no existe")
+
+console.log(numeros_repetidos)
+
+
+// mostrar set con for
+
+for (const element of numeros_repetidos) {
+    console.log(element)
+}
+
+
+// map categoria de producto
+const categoriasProductos = new Map();
+
+// 2. Asignación de datos (Categoría -> Array de nombres)
+categoriasProductos.set('cuero', ['zapato', 'correa']);
+categoriasProductos.set('tela', ['pantalon']);
+
+console.log(categoriasProductos);
+
+
+//listar propiedades y valores del objeto
+
+for (let key in productos) {
+
+    console.log(key,":",productos[key])
+}
+
+// mostrar set con for
+
+for (const element of numeros_repetidos) {
+    console.log(element)
+}
+
+// recorrer map con for each
 
